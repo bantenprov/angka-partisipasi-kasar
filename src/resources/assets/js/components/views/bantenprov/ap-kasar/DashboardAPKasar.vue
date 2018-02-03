69fa8024b3ed8d8a9645c7b2b6077bca0d99fe7c
Igoy Nawamreh <igoynawamreh@gmail.com>
2018-02-03 15:26:06 +0700

    Updates

--- src/resources/assets/js/components/views/bantenprov/ap-kasar/DashboardAPKasar.vue
+++ src/resources/assets/js/components/views/bantenprov/ap-kasar/DashboardAPKasar.vue
@@ -2,42 +2,17 @@
   <div>
 
     <div class="row">
-      <div class="col-md-4 mb-3">
-        <div class="card card-media text-white bg-primary">
+      <div class="col-lg-6 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #2196F3;">
           <div class="card-body">
-            <div class="media">
-              <div class="media-body text-truncate">
-                <div class="card-body">
-                  <ap-kasar-bar-03></ap-kasar-bar-03>
-                </div><!-- /.card-body -->
-              </div>
-            </div>
+            <ap-kasar-bar-03></ap-kasar-bar-03>
           </div><!-- /.card-body -->
         </div><!-- /.card -->
       </div><!-- /col -->
-      <div class="col-md-4 mb-3">
-        <div class="card card-media text-white bg-success">
+      <div class="col-lg-6 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #4CAF50;">
           <div class="card-body">
-            <div class="media">
-              <div class="media-body text-truncate">
-                <div class="card-body">
-                  <ap-kasar-pie-03></ap-kasar-pie-03>
-                </div><!-- /.card-body -->
-              </div>
-            </div>
-          </div><!-- /.card-body -->
-        </div><!-- /.card -->
-      </div><!-- /col -->
-      <div class="col-md-4 mb-3">
-        <div class="card card-media text-white bg-warning">
-          <div class="card-body">
-            <div class="media">
-              <i class="fa fa-calendar fa-card-media-avatar mr-3" aria-hidden="true"></i>
-              <div class="media-body text-truncate">
-                <h5 class="mt-0">Agenda</h5>
-                <span><i class="fa fa-dot-circle-o" aria-hidden="true"></i> 30</span>
-              </div>
-            </div>
+            <ap-kasar-pie-03></ap-kasar-pie-03>
           </div><!-- /.card-body -->
         </div><!-- /.card -->
       </div><!-- /col -->
@@ -46,41 +21,35 @@
     <!-- ===================================================================== -->
 
     <div class="row">
+      <div class="col-lg-8 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #009688;">
+          <div class="card-header">
+            <i class="fa fa-bar-chart" aria-hidden="true"></i>
+            Angka Partisipasi Kasar Berdasarkan Wilayah
+          </div>
+          <div class="card-body">
+            <ap-kasar-bar-01></ap-kasar-bar-01>
+          </div><!-- /.card-body -->
+        </div><!-- /.card -->
+      </div><!-- /col -->
+      <div class="col-lg-4 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #FF9800;">
+          <div class="card-header">
+            <i class="fa fa-pie-chart" aria-hidden="true"></i>
+            Angka Partisipasi Kasar Berdasarkan Wilayah
+          </div>
+          <div class="card-body">
+            <ap-kasar-pie-01></ap-kasar-pie-01>
+          </div><!-- /.card-body -->
+        </div><!-- /.card -->
+      </div><!-- /col -->
+    </div><!-- /.row -->
 
-        <div class="col-md-8">
+    <!-- ===================================================================== -->
 
-          <div class="card mb-3">
-            <div class="card-header">
-              <i class="fa fa-bar-chart" aria-hidden="true"></i>
-              Angka Partisipasi Kasar Berdasarkan Wilayah
-            </div>
-            <div class="card-body">
-              <ap-kasar-bar-01></ap-kasar-bar-01>
-            </div><!-- /.card-body -->
-          </div><!-- /.card -->
-
-        </div><!-- /col -->
-
-
-        <div class="col-md-4">
-
-          <div class="card mb-3">
-            <div class="card-header">
-              <i class="fa fa-pie-chart" aria-hidden="true"></i>
-              Angka Partisipasi Kasar Berdasarkan Wilayah
-            </div>
-            <div class="card-body">
-              <ap-kasar-pie-01></ap-kasar-pie-01>
-            </div><!-- /.card-body -->
-          </div><!-- /.card -->
-
-        </div><!-- /col -->
-      <div class="row">
-
-      </div>
-      <div class="col-md-4">
-
-        <div class="card mb-3">
+    <div class="row">
+      <div class="col-lg-6 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #F44336;">
           <div class="card-header">
             <i class="fa fa-pie-chart" aria-hidden="true"></i>
             Angka Partisipasi Kasar Berdasarkan Tahun
@@ -90,10 +59,8 @@
           </div><!-- /.card-body -->
         </div><!-- /.card -->
       </div><!-- /.col -->
-
-      <div class="col-md-8">
-
-        <div class="card mb-3">
+      <div class="col-lg-6 mb-3">
+        <div class="card text-white bg-dark h-100">
           <div class="card-header">
             <i class="fa fa-bar-chart" aria-hidden="true"></i>
             Angka Partisipasi Kasar Berdasarkan Tahun
@@ -102,48 +69,8 @@
             <ap-kasar-bar-02></ap-kasar-bar-02>
           </div><!-- /.card-body -->
         </div><!-- /.card -->
-
-      </div>
-      <div class="col-md-12">
-        <div class="card mb-3">
-          <div class="card-header">
-            <i class="fa fa-table" aria-hidden="true"></i>
-            Table
-          </div>
-          <div class="card-body">
-            <table class="table mb-0">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Date</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Muhammad Ibnu Musa Al-Khawarizmi</td>
-                  <td>780–850</td>
-                </tr>
-                <tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Abu Ali Al-Husein Ibnu Sina</td>
-                  <td>986-1037</td>
-                </tr>
-                  <th scope="row">3</th>
-                  <td>Abu Nasir Al-Farabi</td>
-                  <td>870-900</td>
-                </tr>
-              </tbody>
-            </table>
-          </div><!-- /.card-body -->
-        </div><!-- /.card -->
-
       </div><!-- /col -->
-      </div>
     </div><!-- /.row -->
 
   </div>
 </template>
-
